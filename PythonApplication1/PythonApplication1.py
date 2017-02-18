ed2ada4b0f2e66557d566553c006228623e63be6
shuishangdz <578314942@qq.com>
2017-02-18 13:43:05 +0800

    haha

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,21 @@
+#print("hello world")
+#name="hello world"
+#print(name[2])
+
+#num=333
+#num2=444
+#print("this is %d,,,,%d" % (num,num2))
+
+
+#���庯��
+def funcName():
+    print("wergwe")
+
+'''
+
+'''
+iggggggg=234786
+
+funcName()
+aaa=callable(funcName)#
+print(ig)
